de61da1d9d78b45370d1f3a3ed61dc57d1441720
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-08-27 16:57:24 +0530

    modified second largest program

--- secondLargest.cpp
+++ secondLargest.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int secondLargest(vector<int> arr, int n) {
-    int largest = INT_MIN, slargest = INT_MIN;
+    int largest = INT_MIN, sLargest = INT_MIN;
 
     for(int i=0; i<n; ++i) {
         if(arr[i] > largest) {
-            slargest = largest;
+            sLargest = largest;
             largest = arr[i];
         }
         else if(arr[i] < largest) {
-            if(arr[i] > slargest)
-                slargest = arr[i];
+            if(arr[i] > sLargest)
+                sLargest = arr[i];
         }
     }
-    if(slargest != INT_MIN)
-        return slargest;
+    if(sLargest != INT_MIN)
+        return sLargest;
     return -1;
 }
 
