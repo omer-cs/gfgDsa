cee626500b1e423ac31423780a66232e4c205d4e
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-08-29 01:35:00 +0530

    added program to find all permutations of a given string

---
+++ allPermutationsOfString.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void permute(string &str, int l, int r) {
+    if(l==r) {
+        cout<<str<<" ";
+        return;
+    }
+    else {
+        for(int i=l; i<=r; ++i) {
+            swap(str[l], str[i]);
+            permute(str, l+1, r);
+            swap(str[l], str[i]);
+        }
+    }
+}
+
+int main() {
+
+    string str;
+    cin>>str;
+
+    int n = str.length();
+    permute(str, 0, n-1);
+    cout<<endl;
+    return 0;
+}
