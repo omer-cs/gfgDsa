b0e82996681c752467d74cfa322e8847d39e4066
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-08-29 00:56:05 +0530

    modified all subsets of a string problem

--- allSubsetsOfString.cpp
+++ allSubsetsOfString.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void allSubsets(string str, string curr="", int index = 0) {
+void allSubsets(string &str, string curr="", int index = 0) {
 
     if(index == str.length()) {
         cout<<curr<<" ";
