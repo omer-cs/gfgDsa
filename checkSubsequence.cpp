c4102577ab5606c53500846d72bd3d5af4bc5ac3
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-09-24 00:28:06 +0530

    added recursive solution for checking subsequence problem

--- checkSubsequence.cpp
+++ checkSubsequence.cpp
@@ -2,6 +2,27 @@
 #define endl '\n'
 using namespace std;
 
+bool checkRecur(string s1, string s2, int i=0, int j=0) {
+    if(j==s2.length())
+        return true;
+    if(i==s1.length())
+        return false;
+    if(s1[i] == s2[j]){
+        return checkRecur(s1, s2, i+1, j+1);
+    }
+    return checkRecur(s1, s2, i+1, j);
+}
+
+bool checkRecurMod(string s1, string s2, int n, int m) {    // this solution does require us to calculate the length of the string since we are checking from the end
+    if(m==0)
+        return true;
+    if(n==0)
+        return false;
+    if(s1[n-1]==s2[m-1])
+        return checkRecurMod(s1, s2, n-1, m-1);
+    return checkRecurMod(s1, s2, n-1, m);   
+}
+
 bool checkItr(string s1, string s2) {
     int n = s1.length();
     int m = s2.length();
@@ -25,6 +46,7 @@ int main() {
     cin>>s1>>s2;
 
     cout<<checkItr(s1, s2)<<endl;
-
+    cout<<checkRecur(s1, s2)<<endl;
+    cout<<checkRecurMod(s1, s2, s1.length(), s2.length())<<endl;
     return 0;
 }
