2aeb25956e0366fa09d0d39c8a736d5d709eb36b
Syed Omer Farooq <syedomer.cse@gmail.com>
2022-10-07 23:32:12 +0530

    program to sort an array using insertion sort

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -11,21 +11,32 @@ void printArray(vector<int>& arr) {
     cout<<endl;
 }
 
+
 void insertionSort(vector<int>& arr) {
     int n = arr.size();
-    for(int i=1; i<n; ++i) {
-        int key = arr[i];
-        int j=i-1;
-        while(j>0 && arr[j]>key) {
-            arr[j+1] = arr[j];
-            j--;
+    int key, j;
+    for(int i=1; i<n; ++i) {            // we start from the second element
+        key = arr[i];                   // key so that we can put the key in correct position in sorted part of array
+        j = i-1;                        // we go one step back from current index
+        while(j>=0 && arr[j]>key) {     // we continue till we reach the 0th index or we arrive at correct postion of key
+            arr[j+1] = arr[j];          // while going till the correct postion we move the copy previous element to the present element
+            j--;    
         }
-        arr[j+1] = key;
+        arr[j+1] = key;                 // we are at correct position of key so we replace arr[j+1] with key
     }
 }
 
 int main() {
     fast_cin();
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; ++i) {
+        cin>>arr[i];
+    }
 
+    insertionSort(arr);
+
+    printArray(arr);
     return 0;
 }
